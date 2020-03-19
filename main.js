9b46ce4200bce4491e82631989a8df8eb5365656
Colin Jaffe <balloonasaurus@gmail.com>
2020-03-19 15:41:04 -0400

    Adds updated solution.

--- main.js
+++ main.js
@@ -1,35 +1,68 @@
-// Make your global array here.
+// Make your global todos array here.
 // Put a few todos in there to start with!
-// This is mostly for testing purposes.
+// This is mostly to give us some data to test our functions with.
 const todos = [
   `Be able to mark todos "done".`,
   `Allow user interaction through the DOM`,
   `Add dates to todos.`,
 ];
 
-// A function that prints a todo.
-// For now, just console log it!
+// Now test it here just by console.logging a particular todo index.
+console.log(todos[1]);
+
+// Next: a function that prints a todo.
+// For now, just have this function console log the value passed in!
+// (We'll expand this helper later.)
 function printTodo(todo) {
   console.log(todo);
 }
 
-// A function that prints everything on our todo list, INDIVIDUALLY.
-// Make SURE to use the above function!
+// Below, call your printTodo with a value of any kind. All it will do is console.log it,
+// but soon we'll do more with it!
+printTodo('do stuff good');
+
+// Now write a function that prints everything on our todo list, INDIVIDUALLY.
+// Make SURE to use the above printTodo function!
 function printList() {
-  let i = 0;
-  while (i < todos.length) {
-    printTodo(todos[i]);
-    i = i + 1;
+  for (const todo of todos) {
+    printTodo(todo);
   }
 }
 
-// A function that adds an item to our todo list.
-// Where on the list should we add it?
+//Now test it out by calling your print-everything function. It should print out each item from your todo list array.
+printList();
+
+
+// Now please write a function that adds an item to our todo list.
+// Could add it anywhere! We'll talk about where it should be added eventually.
 function addTodo(todo) {
   todos.push(todo);
 }
 
-// A function that removes an item at a given index from our todo list.
+// Test the above function by calling it with a value,
+// then calling your print-everything function and making sure it's in there.
+addTodo('make more todo apps');
+printList();
+
+
+// Now write a function that removes an item at a given index from our todo list.
 function removeTodo(i) {
   todos.splice(i, 1);
 }
+
+// Test the above function by passing it an index, then calling our print-everything
+// function to see if the correct thing was removed.
+removeTodo(0);
+printList();
+
+
+// Lastly: write a function that clears all items from our array.
+function clearAll() {
+  todos.splice(0);
+}
+
+// Test it by calling it and then passing your todos to console log.
+// It should be an empty array now!
+clearAll();
+console.log(todos);
+printList();
